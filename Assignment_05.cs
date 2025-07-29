06067bdbf9d9ed02ce526a649f0bb51f5b1581ee
kartikl768 <kartikl6152768@gmail.com>
2025-07-29 23:32:22 +0530

    Update Assignment_05.cs

--- Assignment_05.cs
+++ Assignment_05.cs
@@ -1,108 +1,158 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 /*
  * Create a CRUD based App for developing a Movie Database software where the user can add, remove and update movies of his Video library. It should be a menu driven program that has 4 use cases for add, removing, finding and updating movie info in the application.
  */
+
 namespace Assignment_solutions
 {
-    internal class Assignment_05
+    internal class asgn_05
     {
-        
-        static List<string> movies = new List<string>();
+        class Movie
+        {
+            public int id;
+            public string name;
+            public int year;
+
+            public int MovieId
+            {
+                get { return id; }
+                set { id = value; } // value is sp keyword
+            }
+            public string Moviename
+            {
+                get { return name; }
+                set { name = value; }
+
+            }
+            public int Movieyear
+            {
+                get { return year; }
+                set { year = value; }
+            }
+        }
+        class moviesRepo
+        {
+            static List<Movie> movies = new List<Movie>();
+            public void AddMovie(Movie mv)
+            {
+                movies.Add(mv);
+                Console.WriteLine($"{mv} movie added.");
+            }
+            public void RemoveMovie(Movie mv)
+            {
+                movies.Remove(mv);
+                Console.WriteLine($"{mv} is removed");
+            }
+            public void UpdateMovie(Movie mv)
+            {
+                for(int i=0;i<movies.Count;i++)
+                {
+                    if (movies[i].id==mv.id)
+                    {
+                        movies[i] = new Movie(); 
+                        movies[i].id = mv.id;
+                        movies[i].name = mv.name;
+                        movies[i].year = mv.year;
+                        Console.WriteLine($"{mv.name} updated.");
+                        return;
+                    }
+
+                }
+            }
+            public Movie Findmovie(int id)
+            {
+                return movies.Find(mv => mv.id == id);
+            }
+            public void GetAllMovies()
+            {
+                foreach (var movie in movies)
+                {
+                    Console.WriteLine($"id: {movie.id} name: {movie.name} year: {movie.year}");
+                }
+            }
+           
+            public void DeleteMovie(int id)
+            {
+                var rec = movies.Find(mv => mv.id == id);
+                if (rec == null)
+                {
+                    Console.WriteLine($"No MOvie found with ID {id} to delete.");
+                    return;
+                }
+                movies.Remove(rec);
+                Console.WriteLine("Movie deleted.");
+            }
+        }
+
+        static moviesRepo repo = new moviesRepo();
         static void Main(string[] args)
         {
-
+            bool processing = true;
             do
             {
-                Console.WriteLine("enter the Choice");
-                Console.WriteLine(" 1 add\n 2. removing\n 3. finding\n 4. updating\n 5. List All Movies");
-                int ch = Convert.ToInt32(Console.ReadLine());
-                switch (ch)
-                {
-                    case 1: addnewMovie(); break;
-                    case 2: removeMovie(); break;
-                    case 3: FindMovie(); break;
-                    case 4: UpdateMovie(); break;
-                    case 5: ListAllMovie(); break;
-                    
-                }  
-            } while (true);
-            
-            
+                Console.WriteLine("Enter the choice:");
+                Console.WriteLine("1. ADD\n 2.Remove\n 3.Update\n 4.find\n 5.List all");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                processing = processMenu(choice);
+            }while(processing);
         }
-
-        
-
-        private static void ListAllMovie()
+        private static bool processMenu(int choice)
         {
-            foreach(var movie in movies)
+            switch (choice)
             {
-                Console.WriteLine(movie);
+                case 1: addmovie(); return true;
+                case 2: removemovie(); return true;
+                case 3: updatemovie(); return true;
+                case 4: findmovie(); return true;
+                case 5: listallmovies(); return true;
+                default: Console.WriteLine("Invalid Choice"); return false;
             }
         }
 
-        private static void UpdateMovie()
+        private static void listallmovies()
         {
-            Console.WriteLine("Enter Name of the movie to update: ");
-            string movie = Console.ReadLine();
-            Console.WriteLine("Enter the new name");
-            string newmovie = Console.ReadLine();
-            int index= movies.IndexOf(movie);
-            if(index != -1)
-            {
-                movies[index] = newmovie;
-                Console.WriteLine($"{movie} updated to {newmovie} successfully");
-            }
-            else
-            {
-                Console.WriteLine("Movie didn't exit in the Database");
-            }
+            repo.GetAllMovies();
         }
 
-        private static void removeMovie()
+        private static void updatemovie()
         {
-            Console.WriteLine("Enter Name of the movie to remove: ");
-            string movie = Console.ReadLine();
-            if(movies.Contains(movie))
-            {
-                movies.Remove(movie);
-                Console.WriteLine($"{movie} removed Successfully");
-            }
-            else
-            {
-                Console.WriteLine("Movie didn't exit in the database");
-            }
+            Movie mv = new Movie();
+            Console.WriteLine("Enter the movie id");
+            int mvid= int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the movie name");
+            string name= Console.ReadLine();
+            Console.WriteLine("Enter the movie Year");
+            int year= int.Parse(Console.ReadLine());
+            repo.UpdateMovie(mv);
         }
 
-        private static void FindMovie()
+        private static void findmovie()
         {
-            Console.WriteLine("Enter the Movie name:");
-            string movie= Console.ReadLine();
-            //string foundMovie = movies.Find(m => m.Equals(movie, StringComparison.OrdinalIgnoreCase));
-            if (movies.Contains(movie) )
-            {
-                Console.WriteLine($"Movie found in the Library: {movie}");
-            }
-            else
-            {
-                Console.WriteLine($"Can't find {movie} in the library");
-            }
+            Console.WriteLine("Enter the movie id to find");
+            int mvid=int.Parse(Console.ReadLine()); 
+            repo.Findmovie(mvid);
         }
 
-        private static void addnewMovie()
+        private static void removemovie()
         {
-            Console.WriteLine("Enter the new movie");
-            string movie = Console.ReadLine();
-            if(movies.Contains(movie))
-            {
-                Console.WriteLine("MOvie already exits in the movie Database");
-                return ;
-            }
-            movies.Add(movie);
-            Console.WriteLine("Movie added successfully");
+            Console.WriteLine("Enter the movie id to remove:");
+            int mid=int.Parse(Console.ReadLine());  
+            repo.DeleteMovie(mid);
+        }
+
+        private static void addmovie()
+        {
+            Movie mv = new Movie();
+
+            mv.id=int.Parse(Console.ReadLine());
+            mv.name = Console.ReadLine();
+            mv.year = int.Parse(Console.ReadLine());
+            repo.AddMovie(mv);
         }
     }
 }
